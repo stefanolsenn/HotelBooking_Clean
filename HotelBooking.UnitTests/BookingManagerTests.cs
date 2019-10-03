446cfc9722217ec8aca6d1e3f1c06fe26b1617a2
Stefan <so@tutee.dk>
2019-10-03 10:44:07 +0200

    CreatingBooking test

--- HotelBooking.UnitTests/BookingManagerTests.cs
+++ HotelBooking.UnitTests/BookingManagerTests.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using HotelBooking.Core;
-using HotelBooking.UnitTests.Fakes;
 using Moq;
 using Xunit;
 
@@ -9,11 +8,11 @@ namespace HotelBooking.UnitTests
 {
     public class BookingManagerTests
     {
-
         private readonly Mock<IRepository<Room>> _roomRepoMock;
         private readonly Mock<IRepository<Booking>> _bookingRepoMock;
 
-        public BookingManagerTests(){
+        public BookingManagerTests()
+        {
             _roomRepoMock = new Mock<IRepository<Room>>();
             _bookingRepoMock = new Mock<IRepository<Booking>>();
         }
@@ -22,21 +21,24 @@ namespace HotelBooking.UnitTests
         /// Black dates always fails
         /// </summary>
         /// <returns></returns>
-        public static IEnumerable<object[]> GetBlackDates() {
-            yield return new object[] { DateTime.Today.AddDays(10), DateTime.Today };
-        }
-        
-        public static IEnumerable<object[]> GetWhiteDates() {
-            yield return new object[] { DateTime.Today.AddDays(5), DateTime.Today.AddDays(9) };
-            yield return new object[] { DateTime.Today.AddDays(21), DateTime.Today.AddDays(25) };
+        public static IEnumerable<object[]> ConflictingDates()
+        {
+            yield return new object[] {DateTime.Today.AddDays(10), DateTime.Today};
         }
 
-        public static IEnumerable<object[]> GetBrownDates() {
-            yield return new object[] { DateTime.Today.AddDays(10), DateTime.Today.AddDays(20) };
+        public static IEnumerable<object[]> NonConclitingDates()
+        {
+            yield return new object[] {DateTime.Today.AddDays(5), DateTime.Today.AddDays(9)};
+            yield return new object[] {DateTime.Today.AddDays(21), DateTime.Today.AddDays(25)};
         }
-        
+
+        public static IEnumerable<object[]> OverlappingDates()
+        {
+            yield return new object[] {DateTime.Today.AddDays(10), DateTime.Today.AddDays(20)};
+        }
+
         [Theory]
-        [MemberData(nameof(GetBlackDates))]
+        [MemberData(nameof(ConflictingDates))]
         public void FindAvailableRoom_StartDateNotInTheFuture_ThrowsArgumentException(DateTime start, DateTime end)
         {
             // Arrange
@@ -44,16 +46,16 @@ namespace HotelBooking.UnitTests
             //Assert
             Assert.Throws<ArgumentException>(() => manager.FindAvailableRoom(start, end));
         }
-        
+
         [Theory]
-        [MemberData(nameof(GetWhiteDates))]
-        public void FindAvailableRoom_BookingPeriodeIsBeforeAlreadyBookedRooms_ShouldBookARoom(DateTime start, DateTime end)
+        [MemberData(nameof(NonConclitingDates))]
+        public void FindAvailableRoom_BookingPeriodeIsBeforeAlreadyBookedRooms_ShouldBookARoom(DateTime start,
+            DateTime end)
         {
             // Arrange
             _bookingRepoMock.Setup(repo => repo.GetAll())
                 .Returns(new List<Booking>()
                 {
-
                     new Booking
                     {
                         Id = 1, StartDate = DateTime.Today.AddDays(10), EndDate = DateTime.Today.AddDays(20),
@@ -71,23 +73,22 @@ namespace HotelBooking.UnitTests
                     new Room() {Description = "hejhej", Id = 3},
                 });
             var manager = CreateInstance();
-            
+
             // Act
             var result = manager.FindAvailableRoom(start, end);
-            
+
             // Assert
             Assert.Equal(3, result);
         }
-        
+
         [Theory]
-        [MemberData(nameof(GetBrownDates))]
+        [MemberData(nameof(OverlappingDates))]
         public void FindAvailableRoom_RoomIsBookedInPeriod_ShouldNotBookARoom(DateTime start, DateTime end)
         {
             // Arrange
             _bookingRepoMock.Setup(repo => repo.GetAll())
                 .Returns(new List<Booking>()
                 {
-
                     new Booking
                     {
                         Id = 1, StartDate = DateTime.Today.AddDays(10), EndDate = DateTime.Today.AddDays(20),
@@ -106,18 +107,61 @@ namespace HotelBooking.UnitTests
                     new Room() {Description = "hejhej", Id = 1},
                 });
             var manager = CreateInstance();
-            
+
             // Act
             var result = manager.FindAvailableRoom(start, end);
-            
+
             // Assert
             Assert.Equal(-1, result);
+        }
+
+
+        public static IEnumerable<object[]> Bookings()
+        {
+            yield return new object[]
+            {
+                new Booking()
+                {
+                    RoomId = 3,
+                    IsActive = false,
+                    StartDate = DateTime.Today.AddDays(5),
+                    EndDate = DateTime.Today.AddDays(9)
+                },
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(Bookings))]
+        public void CreateBooking_RoomIdIsThree_ShouldAddBooking(Booking booking)
+        {
+            // Arrange
+            _bookingRepoMock.Setup(repo => repo.GetAll())
+                .Returns(new List<Booking>()
+                {
+                    new Booking
+                    {
+                        Id = 2, StartDate = DateTime.Today.AddDays(10), EndDate = DateTime.Today.AddDays(20),
+                        IsActive = true, CustomerId = 2, RoomId = 2
+                    },
+                });
+            _roomRepoMock.Setup(repo => repo.GetAll())
+                .Returns(new List<Room>()
+                {
+                    new Room() {Description = "hejhej", Id = 3},
+                });
+            var manager = CreateInstance();
+
+            // Act
+            var result = manager.CreateBooking(booking);
+
+            // Assert
+            Assert.True(result);
+            _bookingRepoMock.Verify(x => x.Add(It.IsAny<Booking>()), Times.Once);
         }
 
         public IBookingManager CreateInstance()
         {
             return new BookingManager(_bookingRepoMock.Object, _roomRepoMock.Object);
         }
-
     }
 }
